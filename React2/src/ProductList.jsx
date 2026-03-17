e9f60c51505fe0cbc2d255b82b9ba858b1c961cb
Abhi_Raj <abhishubhraj111@gmail.com>
2026-03-18 00:54:23 +0530

    conditionals

--- React2/src/ProductList.jsx
+++ React2/src/ProductList.jsx
@@ -1,26 +1,21 @@
 import Product from "./Product.jsx"
 
 function ProductList(){
-    //pre-wrap
-    let features = ["hitech","durable","fast"];
     return (
         <>
             <Product 
                 title="Mobile" 
                 price={30000} 
                 quantity={2}
-                features={features}
             />
             <Product 
                 title="Laptop" 
                 price={45000}
                 quantity={1}
-                features={features}
             />
             <Product 
                 title="Clothes"
                 quantity={1}
-                features={features}
             />
         </>
     )
