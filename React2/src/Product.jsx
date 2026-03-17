ba919f8158b03809a3f7e6f6d55ef01c869baac3
Abhi_Raj <abhishubhraj111@gmail.com>
2026-03-17 23:46:55 +0530

    passingArrays

--- React2/src/Product.jsx
+++ React2/src/Product.jsx
@@ -1,11 +1,12 @@
 import "./Product.css"
 
-function Product({title, price = -1, quantity}){
+function Product({title, price = -1, quantity, features}){
     return (
         <div className="Product">
             <h1>Name: {title}</h1>
             <h3>Price: {price}</h3>
             <h3>Quantity: {quantity}</h3>
+            <p>{features}</p>
         </div>
     )
 }
